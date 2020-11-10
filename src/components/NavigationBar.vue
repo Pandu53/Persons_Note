9c8cfc4a8d09d595b617459382d2621f7ea91b50
Kevin Pandura <pandu.kev.kp@gmail.com>
2020-11-10 20:23:03 +0100

    Added Navigation

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav class="nav nav-pills nav-fill">
+      <a class="nav-item nav-link active" href="#">Active</a>
+      <a class="nav-item nav-link" href="#">Link</a>
+      <a class="nav-item nav-link" href="#">Link</a>
+      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar",
+  data: function () {
+    return {};
+  },
+};
+</script>
