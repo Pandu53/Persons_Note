2248ec0758197e398be07efadc1a5a1a82124e1e
Kevin Pandura <pandu.kev.kp@gmail.com>
2020-11-12 18:36:54 +0100

    edited Navigation bar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-    <nav class="nav nav-pills nav-fill">
-      <a class="nav-item nav-link active" href="#">Active</a>
-      <a class="nav-item nav-link" href="#">Link</a>
-      <a class="nav-item nav-link" href="#">Link</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
-    </nav>
+  <div class="row">
+    <div class="col-2"><span class="nav-title">Plug Letter</span></div>
+    <div class="col-6">
+      <nav class="nav nav-pills nav-fill">
+        <a class="nav-item nav-link" href="#">List Person</a>
+      </nav>
+    </div>
+    <div class="col-4">
+      <nav class="nav nav-pills nav-fill">
+        <a class="nav-item nav-link" href="#">Add Person</a>
+        <a class="nav-item nav-link" href="#">Logout</a>
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -17,3 +23,10 @@ export default {
   },
 };
 </script>
+
+<style  scoped>
+.nav-title {
+  font-weight: bold;
+  margin: 0 10px;
+}
+</style>
