814a4c9aebd2ba5d9f7a9d7d881938ca50f75e13
Kevin Pandura <pandu.kev.kp@gmail.com>
2020-11-12 19:07:53 +0100

    Added views for PersonsList and AddPerson

---
+++ src/views/AddPerson.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:"AddPerson",
+    data: function(){
+        return{
+            
+        }
+    }
+}
+</script>
---
+++ src/views/PersonsList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:"PersonsList",
+    data: function(){
+        return{
+            
+        }
+    }
+}
+</script>
