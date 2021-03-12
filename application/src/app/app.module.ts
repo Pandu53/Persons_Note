3eef3c9c526c3a436d7456ea45da7d4056e47081
Kevin Pandura <pandu.kev.kp@gmail.com>
2021-03-12 22:00:32 +0100

    feat: :sparkles:

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { StickyNoteComponent } from './sticky-note/sticky-note.component';
+import { CardComponent } from './card/card.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    StickyNoteComponent
+    StickyNoteComponent,
+    CardComponent,
+    TopBarComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule
